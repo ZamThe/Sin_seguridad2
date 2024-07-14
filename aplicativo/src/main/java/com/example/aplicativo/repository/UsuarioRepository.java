0eb18e4c50c3889336a0f46e28d3b17ce3ad34ae
ZamThe <jhoan.zamudio@est.iudigital.edu.co>
2024-07-13 20:19:31 -0500

    correcion
    correcion

--- aplicativo/src/main/java/com/example/aplicativo/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.aplicativo.repository;
-
-import com.example.aplicativo.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-    Usuario findByUsername(String username);
-}
